b5f5e73d7859caf604c2d5a4051df03163018341
Wellington <welltecnic@hotmail.com>
2026-09-15 09:56:09 -0300

    feat: codificando e estilizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import Produtos from './pages/Produtos'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <NavBar/>
+        <main>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/sobre" element={<Sobre/>}/>
+              <Route path="/contato" element={<Contato/>}/>
+              <Route path="/produtos" element={<Produtos/>}/>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,27 @@
-
+import {Link} from 'react-router-dom'
+import {Home,Info,Mail,Package} from 'lucide-react'
 
 const NavBar = () => {
   return (
-    <div>
+    <nav className="bg-blue-600 text-white shadow-lg">
+      <div className='max-w-6xl mx-auto px-4'>
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex items-center space-x-2">
+            <span className="text-2xl font-bold ">Projeto</span>
+          </div>
+
+          <div className="hidden md:flex space-x-2 items-center">
+            <Link to="/"className="flex items-center gap-2 py-2 rounded-2xl"><Home />Home</Link>
+            <Link to="/sobre" className="flex items-center gap-2 py-2 rounded-2xl"><Info />Sobre</Link>
+            <Link to="/produtos" className="flex items-center gap-2 py-2 rounded-2xl"><Package />Produtos</Link>
+            <Link to="/contato" className="flex items-center gap-2 py-2 rounded-2xl" ><Mail />Contato</Link>
+          </div>
+        </div>
+
+        </div>
+        
       
-    </div>
+    </nav>
   )
 }
 
